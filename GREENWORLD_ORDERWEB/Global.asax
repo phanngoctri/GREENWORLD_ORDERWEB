26dff8e51fa5097d9b81642d4ea5e306de7d6743
Tri Phan Ngoc <43377863+phanngoctri@users.noreply.github.com>
2021-07-23 12:07:49 +0700

    Add project files.

---
+++ GREENWORLD_ORDERWEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GREENWORLD_ORDERWEB.MvcApplication" Language="C#" %>
---
+++ GREENWORLD_ORDERWEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GREENWORLD_ORDERWEB.Startup))]
+namespace GREENWORLD_ORDERWEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
